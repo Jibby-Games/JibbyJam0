2529712bc0a11b97e49a06b6968949bfa4037b7e
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-03-20 20:20:45 +0000

    add bullet firing

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,16 +1,12 @@
-extends KinematicBody2D
+extends Area2D
+
+var speed = 750
+
+func _physics_process(delta):
+    position += transform.x * speed * delta
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _on_bullet_body_entered(body:Node) -> void:
+    if body.is_in_group("enemies"):
+        body.queue_free()
+    queue_free()
