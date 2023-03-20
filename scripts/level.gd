05ea727185a7eabaf02f40d44dcf6ca549298843
James Wild <wild.james343@gmail.com>
2023-03-20 23:14:37 +0000

    Add glitch to main. Level stuff

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var player_pawn
 var death_label
+var restart_label
 
 var player_char
 var camera
@@ -10,6 +11,7 @@ var camera
 func _ready() -> void:
 	player_pawn = get_node("player")
 	death_label = get_node("camera/death_label")
+	restart_label = get_node("camera/restart_label")
 	
 	player_char = preload("res://player.tscn")
 
@@ -17,9 +19,11 @@ func _process(delta):
 	
 	if !is_instance_valid(player_pawn):
 		death_label.visible = true
+		restart_label.visible = true
 	else:
 		death_label.visible = false
+		restart_label.visible = false
 	
 	if death_label.visible:
-		if Input.is_action_pressed("shoot"):
+		if Input.is_action_just_pressed("restart"):
 			get_tree().change_scene("res://level.tscn")
