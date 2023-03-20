06d4e2df5a14e5535a68d5d58aa58d4e46094186
James Wild <wild.james343@gmail.com>
2023-03-20 22:52:04 +0000

    Death screen

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,5 +1,27 @@
 extends Node2D
 
+var player_pawn
+var death_label
+
+var player_char
+var camera
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	player_pawn = get_node("player")
+	death_label = get_node("camera/death_label")
+	
+	player_char = preload("res://player.tscn")
+
+func _process(delta):
+	
+	if !is_instance_valid(player_pawn):
+		death_label.visible = true
+	else:
+		death_label.visible = false
+	
+	if death_label.visible:
+		if Input.is_action_pressed("shoot"):
+			var new_player = player_char.instance()
+			
+			add_child(new_player)
