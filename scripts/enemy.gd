262395ad94dae1a2a55a810ed0ba7f61df8761bb
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-03-20 20:41:14 +0000

    fix and add powerup

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -13,23 +13,15 @@ func _ready():
 
 func get_heading():
 	var heading = Vector2()
-<<<<<<< HEAD
-	
+
 	var target = Vector2()
 	if is_instance_valid(player_pawn):
 		target = player_pawn.global_position
 	else:
 		target = self.global_position
-	
+
 	heading = target - self.global_position
-	
-=======
 
-	var player_loc = player_pawn.global_position
-
-	heading = player_loc - self.global_position
-
->>>>>>> 151712cf7c074ecebdecdc8c37f56ab8d5c3007c
 	return heading
 
 func _physics_process(delta):
@@ -43,8 +35,4 @@ func _physics_process(delta):
 		velocity = lerp(velocity, Vector2.ZERO, friction)
 
 	velocity = move_and_slide(velocity)
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> 151712cf7c074ecebdecdc8c37f56ab8d5c3007c
