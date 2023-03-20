abbcc275b9e1e27bda84ea2165a2d0101f23e803
James Wild <wild.james343@gmail.com>
2023-03-20 20:10:44 +0000

    Enemy chases player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,16 +1,34 @@
 extends KinematicBody2D
 
+export var speed = 150
+export var friction = 0.01
+export var acceleration = 0.1
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2()
+var player_pawn
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player_pawn = get_parent().get_node("player")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_heading():
+	var heading = Vector2()
+	
+	var player_loc = player_pawn.global_position
+	print(player_loc)
+	
+	heading = player_loc - self.global_position
+	
+	return heading
+
+func _physics_process(delta):
+	var direction = get_heading()
+	
+	look_at(direction)
+	
+	if direction.length() > 0:
+		velocity = lerp(velocity, direction.normalized() * speed, acceleration)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, friction)
+		
+	velocity = move_and_slide(velocity)
