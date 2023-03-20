151712cf7c074ecebdecdc8c37f56ab8d5c3007c
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-03-20 20:29:29 +0000

    fix player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -13,24 +13,22 @@ func _ready():
 
 func get_heading():
 	var heading = Vector2()
-	
+
 	var player_loc = player_pawn.global_position
-	
+
 	heading = player_loc - self.global_position
-	
+
 	return heading
 
 func _physics_process(delta):
 	var direction = get_heading()
-	
+
 	look_at(direction)
-	
+
 	if direction.length() > 0:
 		velocity = lerp(velocity, direction.normalized() * speed, acceleration)
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, friction)
-		
+
 	velocity = move_and_slide(velocity)
-	
-	if player_pawn.is_colliding():
-		print("Player Collision!")
+
